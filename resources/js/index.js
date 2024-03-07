1d1ba5cc49f930253210d41d662e26bc79d200b6
phun <phun5701@gmail.com>
2024-03-06 18:13:51 -0600

    Added json files, small changes

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,26 +1,3 @@
-/*
-        <div class="team-container">
-            <div class="team-container-title">
-                <img class="team-logo" src="https://owcdn.net/img/603c00d5c5a08.png" alt="100-thieves">
-                <div class="team-info">
-                    <a class="team-name" href="#">100 Thieves <img src="./resources/img/US.jpg" alt="US flag" width="25px" height="15px"></a>
-                    <div class="team-stats"> 
-                        <ul>
-                            <li><strong>MATCHES WON: </strong></li>
-                            <li><strong>MATCHES LOST: </strong></li>
-                            <li><strong>MATCH WIN%: </strong></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="team-container-content-wrapper">
-                <div class="team-container-content">
-                    <p>PLAYER 1. </p>
-                </div>
-            </div>
-        </div>
-*/
-
 var teams = [ "100t", "c9", "eg", "fur", "g2", "kru", "lev", "loud", "mibr", "nrg", "sen" ];
 
 for (let i = 0; i < teams.length; i++) {
@@ -37,6 +14,7 @@ for (let i = 0; i < accordion.length; i++) {
         this.classList.toggle('active');
     })
 }
+
 function loadTeam(team) {
     var teamName = team.data.name;
     var teamLogo = team.data.logo;
@@ -65,7 +43,7 @@ function loadTeam(team) {
             <div class="team-container-content">
                 <p>PLAYER 1. </p>
             </div>
-        </div>  
+        </div>
     `;
     teamContainer.children[0].addEventListener('click', function () {
         this.nextElementSibling.classList.toggle('active');
