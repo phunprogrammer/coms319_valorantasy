c0e1ad877ce8346b2eb34920ef79bb0673f3204d
phun <phun5701@gmail.com>
2024-05-01 17:50:36 -0500

    Seperated auth into controller and service

---
+++ backend/models/teamModel.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+const teamSchema =  new mongoose.Schema({
+    name: { type: String, required: true }
+});
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,40 +1,15 @@
 const router = require('express').Router();
 const User = require('../models/userModel');
-const bcrypt = require('bcryptjs');
+const { 
+    register,
+    login,
+    logout
+} = require('../controllers/authController.js');
 
-router.post('', async (req, res) => {
-    try {
-        const { username, password } = req.body;
+router.post('/register', register);
 
-        if(!username || !password) 
-            return res.status(400).json( {errorMessage: "Invalid Fields"} );
+router.post('/login', login);
 
-        if(password.length < 8)
-            return res.status(400).json( {errorMessage: "Password too short"} );
-
-        const existingUser = await User.findOne( { username } );
-
-        if(existingUser)
-            return res.status(400).json( {errorMessage: "Username taken"} );
-
-        const salt = await bcrypt.genSalt();
-        const passwordHash = await bcrypt.hash(password, salt);
-
-        const newUser = new User({
-            username, passwordHash
-        });
-
-        const savedUser = await newUser.save();
-        console.log(savedUser);
-    }
-    catch(err) {
-        console.error(err);
-        res.status(500).send();
-    }
-});
-
-router.get('/register', (req, res) => {
-    res.send('this is register route');
-});
+router.get('/logout', logout);
 
 module.exports = router;
